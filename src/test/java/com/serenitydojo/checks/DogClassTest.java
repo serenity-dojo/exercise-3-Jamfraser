1f4b22fae4e4bdcee7730c357ce69de3b3c0b0c4
Jamfraser <jamfraser@gmail.com>
2023-11-29 15:10:39 -0500

    Exercise Completed
    Added makeNoise() method.
    Added feed() method.
    Refactored to have makeNoise() use static constant DOG_NOISE.
    
    Added unit tests to verify the above.

--- src/test/java/com/serenitydojo/checks/DogClassTest.java
+++ src/test/java/com/serenitydojo/checks/DogClassTest.java
@@ -1,8 +1,12 @@
 package com.serenitydojo.checks;
 
+import com.serenitydojo.Dog;
 import org.junit.Test;
 
+import java.lang.reflect.InvocationTargetException;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
 
 public class DogClassTest {
     @Test
@@ -24,4 +28,27 @@ public class DogClassTest {
     public void theDogClassShouldHaveAnAge() throws Exception {
         assertThat(Class.forName("com.serenitydojo.Dog").getDeclaredField("age").getGenericType()).isEqualTo(int.class);
     }
+
+    @Test
+    public void theDogShouldMakeNoise() throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Dog dog = new Dog("Buddy", "Ball", 3);
+        // Use assertThat to check if the output matches the expected value
+        assertThat(Class.forName("com.serenitydojo.Dog").getMethod("makeNoise").invoke(dog)).isEqualTo("Woof");
+
+    }
+
+    @Test
+    public void theDogShouldBeFedAfterFeeding() throws ClassNotFoundException, NoSuchMethodException, SecurityException, InvocationTargetException, IllegalAccessException {
+        Dog dog = new Dog("Rex", "Bone", 5);
+
+        // Initially, the dog should not be fed
+        assertThat(dog.getFedStatus()).isFalse();
+
+        // Call the feed() method
+        Class.forName("com.serenitydojo.Dog").getMethod("feed").invoke(dog);
+
+        // After feeding, the dog should be fed
+        assertThat(dog.getFedStatus()).isTrue();
+    }
+
 }
